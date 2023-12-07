b847250548c2e6aa43f6c7e9be9f4ad977c310a0
knexxel <ashishkr.xn@gmail.com>
2023-12-07 09:22:19 +0530

    updated

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 const express = require('express')
 const fs = require('fs')
 const cors = require('cors')
+const db = require('./database.json')
 
 const port = 8080;
 const app = express();
 
 app.use(cors());
-app.get('/api',(req,res) => {
+app.get('/api', (req, res) => {
     const id = req.query.id;
-    const db = JSON.parse(fs.readFileSync('database.json','utf8'));
 
-    if(db.hasOwnProperty(id)) {
+    if (db.hasOwnProperty(id)) {
         db[id] += 1;
     }
     else {
         db[id] = 1;
     }
 
-    fs.writeFileSync('database.json',JSON.stringify(db));
-    res.json({count:db[id]});
+    fs.writeFileSync('database.json', JSON.stringify(db));
+    res.json({ count: db[id] });
 })
 
-app.listen(port,() => {
+app.listen(port, () => {
     console.log(`server running at http://localhost:${port}/api`);
 })
